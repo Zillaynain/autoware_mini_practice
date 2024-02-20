c4bf38821f5f429d7b60293cb8b0df710cfd2bfc
Zillay Nain <linuxuser@linux-d2006-18.domenis.local>
2024-02-20 15:58:11 +0200

    Task 3

--- practice_2/nodes/localization/localizer.py
+++ practice_2/nodes/localization/localizer.py
@@ -14,7 +14,7 @@ class Localizer:
     def __init__(self):
 
         # Parameters
-        #self.undulation = rospy.get_param('/undulation')
+        self.undulation = rospy.get_param('/undulation')
         utm_origin_lat = rospy.get_param('/utm_origin_lat')
         utm_origin_lon = rospy.get_param('/utm_origin_lon')
 
@@ -34,13 +34,59 @@ class Localizer:
         self.current_pose_pub = rospy.Publisher('current_pose', PoseStamped, queue_size=10)
         self.current_velocity_pub = rospy.Publisher('current_velocity', TwistStamped, queue_size=10)
         self.br = TransformBroadcaster()
+        
 
     def transform_coordinates(self, msg):
         self.origin_x_b, self.origin_y_b = self.transformer.transform(msg.latitude, msg.longitude)
         print("New", self.origin_x-self.origin_x_b, self.origin_y-self.origin_y_b )
+        # calculate azimuth correction
+        azimuth_correction = self.utm_projection.get_factors(msg.longitude, msg.latitude).meridian_convergence
+        y = convert_azimuth_to_yaw(azimuth_correction)
+        # Convert yaw to quaternion
+        x, y, z, w = quaternion_from_euler(0, 0, y)
+        orientation = Quaternion(x, y, z, w)
+        
+        current_pose_msg = PoseStamped()
+        current_pose_msg.header.stamp = msg.header.stamp
+        current_pose_msg.header.frame_id = "map"
+        current_pose_msg.pose.position.x = self.origin_x-self.origin_x_b
+        current_pose_msg.pose.position.y = self.origin_y-self.origin_y_b
+        current_pose_msg.pose.position.z = msg.height-self.undulation
+        current_pose_msg.pose.orientation = orientation
+        self.current_pose_pub.publish(current_pose_msg)
+
+    # convert azimuth to yaw angle
+    def convert_azimuth_to_yaw(azimuth):
+        """
+        Converts azimuth to yaw. Azimuth is CW angle from the North. Yaw is CCW angle from the East.
+        :param azimuth: azimuth in radians
+        :return: yaw in radians
+        """
+        yaw = -azimuth + math.pi/2
+        # Clamp within 0 to 2 pi
+        if yaw > 2 * math.pi:
+            yaw = yaw - 2 * math.pi
+        elif yaw < 0:
+            yaw += 2 * math.pi
+        return yaw
 
     def run(self):
         rospy.spin()
+        
+# convert azimuth to yaw angle
+def convert_azimuth_to_yaw(azimuth):
+    """
+    Converts azimuth to yaw. Azimuth is CW angle from the North. Yaw is CCW angle from the East.
+    :param azimuth: azimuth in radians
+    :return: yaw in radians
+    """
+    yaw = -azimuth + math.pi/2
+    # Clamp within 0 to 2 pi
+    if yaw > 2 * math.pi:
+        yaw = yaw - 2 * math.pi
+    elif yaw < 0:
+        yaw += 2 * math.pi
+    return yaw
 
 if __name__ == '__main__':
     rospy.init_node('localizer')
