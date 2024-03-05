98acbd9e5ee26114053e13fe0b88389a9f18f7f3
Zillay Nain <linuxuser@linux-d2006-18.domenis.local>
2024-03-05 12:30:56 +0200

    task 5

--- practice_3/nodes/control/pure_pursuit_follower.py
+++ practice_3/nodes/control/pure_pursuit_follower.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 import rospy
+import math
+from tf.transformations import euler_from_quaternion
 
 from autoware_msgs.msg import Lane, VehicleCmd
 from geometry_msgs.msg import PoseStamped
@@ -13,6 +15,10 @@ class PurePursuitFollower:
 
         # Parameters
         self.path_ls = None
+        
+        # Reading in the parameter values
+        self.lookahead_distance = rospy.get_param("~lookahead_distance")
+        self.wheel_base = rospy.get_param("/wheel_base")
         
         # Publishers
         self.vehicle_cmd_pub = rospy.Publisher('/control/vehicle_cmd', VehicleCmd, queue_size=10)
@@ -34,13 +40,21 @@ class PurePursuitFollower:
         vehicle_cmd = VehicleCmd()
         vehicle_cmd.header.stamp = msg.header.stamp
         vehicle_cmd.header.frame_id = "base_link"
-        vehicle_cmd.ctrl_cmd.steering_angle = 0.2
+        vehicle_cmd.ctrl_cmd.steering_angle = steer_ang
         vehicle_cmd.ctrl_cmd.linear_velocity = 10.0
         self.vehicle_cmd_pub.publish(vehicle_cmd)
         
         current_pose = Point([msg.pose.position.x, msg.pose.position.y])
         d_ego_from_path_start = self.path_ls.project(current_pose)
-        print("distance", d_ego_from_path_start)
+        
+        # using euler_from_quaternion to get the heading angle
+        _, _, heading = euler_from_quaternion([msg.pose.orientation.x, msg.pose.orientation.y, msg.pose.orientation.z, msg.pose.orientation.w])
+        
+        # lookahead point heading calculation
+        lookahead_heading = np.arctan2(lookahead_point.y - current_pose.y, lookahead_point.x - current_pose.x)
+        steer_ang = np.arctan2(2*self.wheel_base*(math.sin(heading-self.lookahead_distance))/lookahead_heading)
+        
+        
         
     def run(self):
         rospy.spin()
