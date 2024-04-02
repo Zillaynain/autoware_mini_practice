09144114e8d66a082b1ee28e4871878db64edd5c
Zillay Nain <linuxuser@linux-d2006-18.domenis.local>
2024-04-02 15:21:52 +0300

    Task 4

--- practice_4/nodes/planning/global/lanelet2_global_planner.py
+++ practice_4/nodes/planning/global/lanelet2_global_planner.py
@@ -10,6 +10,7 @@ from lanelet2.projection import UtmProjector
 from lanelet2.core import BasicPoint2d
 from lanelet2.geometry import findNearest
 from geometry_msgs.msg import PoseStamped
+from autoware_msgs.msg import Lane, Waypoint, WaypointState
 
 
 class Lanelet2GlobalPlanner:
@@ -18,6 +19,7 @@ class Lanelet2GlobalPlanner:
     
         # Parameters
         lanelet2_map_name = rospy.get_param("~lanelet2_map_name")
+        self.output_frame = rospy.get_param("~output_frame")
         coordinate_transformer = rospy.get_param("/localization/coordinate_transformer")
         use_custom_origin = rospy.get_param("/localization/use_custom_origin")
         utm_origin_lat = rospy.get_param("/localization/utm_origin_lat")
@@ -25,6 +27,7 @@ class Lanelet2GlobalPlanner:
         
         self.goal_point = None
         self.current_location = None
+        self.waypoints = []
 
         # Load the map using Lanelet2
         if coordinate_transformer == "utm":
@@ -35,8 +38,12 @@ class Lanelet2GlobalPlanner:
         
         # traffic rules
         traffic_rules = lanelet2.traffic_rules.create(lanelet2.traffic_rules.Locations.Germany,lanelet2.traffic_rules.Participants.VehicleTaxi)
+
         # routing graph
         self.graph = lanelet2.routing.RoutingGraph(self.lanelet2_map, traffic_rules)
+
+        # Publishers
+        self.waypoints_pub = rospy.Publisher('global_path', Lane, queue_size=10, latch=True)
     
         # Subscribers
         rospy.Subscriber('/move_base_simple/goal', PoseStamped, self.goal_callback) 
@@ -64,19 +71,48 @@ class Lanelet2GlobalPlanner:
         else:
             # find shortest path
             path = route.shortestPath()
-
             # this returns LaneletSequence to a point where lane change would be necessary to continue
             path_no_lane_change = path.getRemainingLane(start_lanelet)
-            return path_no_lane_change
-        print(path_no_lane_change)
-        
-        
+            waypoints = self.lanelet_sequence_to_waypoints(path_no_lane_change)
+            print(waypoints)
+            self.publish_waypoints(waypoints)
+            
+
     def current_pose_callback(self, msg):
         self.current_location = BasicPoint2d(msg.pose.position.x, msg.pose.position.y)
 
-        
+    def lanelet_sequence_to_waypoints(self,path_no_lane_change): 
+        waypoints = []
+        for lanelet in path_no_lane_change:
+                if 'speed_ref' in lanelet.attributes:
+                    speed_kmh = float(lanelet.attributes['speed_ref'])
+                    speed = speed_kmh / 3.6
+                else:
+                    speed_kmh = rospy.get_param('~speed_limit') 
+                    speed = speed_kmh / 3.6
+                for point in lanelet.centerline:
+                    # Check if it's not the first waypoint or if the previous waypoint is not the same as the current point
+                    if not waypoints or (waypoints[-1].pose.pose.position.x != point.x and waypoints[-1].pose.pose.position.y != point.y):
+                        # create Waypoint and get the coordinats from lanelet.centerline points
+                        waypoint = Waypoint()
+                        waypoint.pose.pose.position.x = point.x
+                        waypoint.pose.pose.position.y = point.y
+                        waypoint.pose.pose.position.z = point.z
+                        waypoint.twist.twist.linear.x = speed
+
+                        waypoints.append(waypoint)
+        return waypoints
+
+    def publish_waypoints(self, waypoints):
+        lane = Lane()      
+        lane.header.frame_id = self.output_frame
+        lane.header.stamp = rospy.Time.now()
+        lane.waypoints = waypoints
+        self.waypoints_pub.publish(lane)
+           
     def run(self):
         rospy.spin()
+     
 
 if __name__ == '__main__':
     rospy.init_node('lanelet2_global_planner')
