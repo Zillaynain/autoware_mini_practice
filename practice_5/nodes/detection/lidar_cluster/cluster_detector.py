4213f93eaf075cba3d43084f87e95a12b5a21890
Zillay Nain <linuxuser@linux-d2006-18.domenis.local>
2024-04-16 16:02:17 +0300

    task6 incomplete

--- practice_5/nodes/detection/lidar_cluster/cluster_detector.py
+++ practice_5/nodes/detection/lidar_cluster/cluster_detector.py
@@ -33,27 +33,62 @@ class ClusterDetector:
 
     def cluster_callback(self, msg):
         data = numpify(msg)
+        label = data['label']
         points = structured_to_unstructured(data, dtype=np.float32)
-        print("-----------------cluster_callback-----------------")
-        print('points_shape before transform:', points.shape)
-        print("first row:", points[0])
+        
         if msg.header.frame_id != self.output_frame:
-            # fetch transform for target frame
             try:
                 transform = self.tf_buffer.lookup_transform(self.output_frame, msg.header.frame_id, msg.header.stamp, rospy.Duration(self.transform_timeout))
             except (TransformException, rospy.ROSTimeMovedBackwardsException) as e:
                rospy.logwarn("%s - %s", rospy.get_name(), e)
                return
         tf_matrix = numpify(transform.transform).astype(np.float32)
-        # make copy of points
         points = points.copy()
-        # turn into homogeneous coordinates
         points[:,3] = 1
-        # transform points to target frame
         points = points.dot(tf_matrix.T)
-        print("tf_matrix:",tf_matrix)
-        print("points_shape after transform:", points.shape)
-        print("first row:", points[0])
+
+        header = Header(**{
+        'stamp': msg.header.stamp,
+        'frame_id': self.output_frame
+        })
+        objects = DetectedObjectArray()
+        
+        if len(points)==0:
+            object.pose.position.x = 0
+            object.pose.position.y = 0
+            object.pose.position.z = 0
+
+        if len(label) == 0:
+            clusters = 0
+        else:
+            clusters = np.max(label) + 1
+            
+        for i in range(clusters):
+            mask = (label == i)
+            if np.sum(mask) < self.min_cluster_size:
+                continue
+            # select points for one object from an array using a mask
+            # rows are selected using a binary mask and only first 3 columns are selected: x, y and z coordinates
+            points3d = points[mask,:3]
+            centroid_x, centroid_y, centroid_z = np.mean(points3d, axis=0)
+            # create convex hull
+            points_2d = MultiPoint(points[mask,:2])
+            hull = points_2d.convex_hull
+            convex_hull_points = [Point32(x, y, centroid_z) for x, y in hull.exterior.coords]
+    
+            object = DetectedObject(header=header)
+            object.pose.position.x = centroid_x
+            object.pose.position.y = centroid_y
+            object.pose.position.z = centroid_z
+            object.id = i
+            object.label = "unknown"
+            object.color = BLUE80P
+            object.valid = True
+            object.space_frame = self.output_frame
+            object.pose_reliable = True
+            object.velocity_reliable = False
+            object.acceleration_reliable = False
+            self.objects_pub.publish(object)
 
     def run(self):
         rospy.spin()
