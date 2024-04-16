fe9589ac266ce854eeb53ebf1e184761c0ef5053
Zillay Nain <linuxuser@linux-d2006-18.domenis.local>
2024-04-16 14:12:23 +0300

    task4

--- practice_5/nodes/detection/lidar_cluster/points_clusterer.py
+++ practice_5/nodes/detection/lidar_cluster/points_clusterer.py
@@ -22,14 +22,11 @@ class PointsCluster:
     def __init__(self):
         self.min_cluster_size = rospy.get_param('~cluster_min_size')
         self.cluster_epsilon = rospy.get_param('~cluster_epsilon')
-        #self.transform_timeout = rospy.get_param('~transform_timeout')
 
-        #self.tf_buffer = Buffer()
-        #self.tf_listener = TransformListener(self.tf_buffer)
-
-        #self.objects_pub = rospy.Publisher('detected_objects', DetectedObjectArray, queue_size=1, tcp_nodelay=True)
+        self.cluster_msg_pub = rospy.Publisher('points_clustered', PointCloud2, queue_size=1, tcp_nodelay=True)
+        
         rospy.Subscriber('points_filtered', PointCloud2, self.points_callback, queue_size=1, buff_size=2**24, tcp_nodelay=True)
-
+        
         rospy.loginfo("%s - initialized", rospy.get_name())
         
         self.cluster = DBSCAN()
@@ -38,15 +35,28 @@ class PointsCluster:
     def points_callback(self, msg):
         data = numpify(msg)
         points = structured_to_unstructured(data[['x', 'y', 'z']], dtype=np.float32)
-        print('point shape:', points.shape)
         
         self.cluster.set_params(eps=self.cluster_epsilon, min_samples=self.min_cluster_size)
         self.cluster.fit_predict(points)
         labels = self.cluster.labels_
-        print('labels shape:', labels.shape)
+
         assert len(points) == len(labels), "Number of points and labels don't match."
+        
+        filtered_points = points[labels != -1]
+        filtered_labels = labels[labels != -1]
+        points_labeled = np.column_stack((filtered_points, filtered_labels))
+        
+        #print('labels shape:', points_labeled)
+        
+        # convert labelled points to PointCloud2 format
+        data = unstructured_to_structured(points_labeled, dtype=np.dtype([('x', np.float32),('y', np.float32),('z', np.float32),('label', np.int32)]))
+        # publish clustered points message
+        cluster_msg = msgify(PointCloud2, data)
+        cluster_msg.header.stamp = msg.header.stamp
+        cluster_msg.header.frame_id = msg.header.frame_id
+        self.cluster_msg_pub.publish(cluster_msg)
 
-
+        
     def run(self):
         rospy.spin()
 
